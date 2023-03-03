39070073ec4a1d0eda42851bd161b981d2b10fd7
SarthakGunjal <92911690+SarthakGunjal@users.noreply.github.com>
2023-03-03 09:54:01 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (29.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,7 +1,45 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int x=haystack.find(needle);
-        return x;
+//         vector<int>s1(26,0);
+//         vector<int>window(26,0);
+//         for(int i=0;i<needle.size();i++){
+//             s1[needle[i]]=1;
+//             window[haystack[i]]=1;
+//         }
+        
+//         if(s1==window)return 0;
+//         int i,j=0;
+        
+// //         for(i=needle.size();i<haystack.size();i++){
+// //             window[haystack[i]]=1;
+// //             window[j]=0;
+// //             j++;
+// //             if(window==s1)return j;
+            
+// //         }
+//         return -1;
+//     }
+    
+    int n = haystack.length();
+    int m = needle.length();
+
+    // edge case
+    if (m > n) {
+        return -1;
     }
-};
+
+    // sliding window
+    for (int i = 0; i <= n - m; i++) {
+        int j = 0;
+        while (j < m && haystack[i+j] == needle[j]) {
+            j++;
+        }
+        if (j == m) {
+            return i;
+        }
+    }
+
+    return -1;
+    }
+    };
