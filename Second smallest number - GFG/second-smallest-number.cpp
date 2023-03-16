5f62126490ce339bb684aa3ae1bd0912a70877d2
SarthakGunjal <92911690+SarthakGunjal@users.noreply.github.com>
2023-03-16 21:40:54 +0530

    Added solution - LeetHub

---
+++ Second smallest number - GFG/second-smallest-number.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+  public:
+    string secondSmallest(int S, int D){
+        // code here 
+        string smallest = "";
+        if(S>=D*9 || D==1) return "-1";
+        for(int i=0; i<D; i++){        // get the smallest number
+            if(S>(D-i-1)*9){
+                smallest+= to_string(S-(D-i-1)*9);
+                S-=S-(D-i-1)*9;
+            }
+            else{
+                if(i==0){
+                    smallest+="1";
+                    S-=1;
+                }
+                else{
+                    smallest+="0";
+                }
+            }
+        }
+        if(smallest[D-1]=='0' || smallest[0]=='9') return "-1";
+        // check for  double 9
+        int i = D-1;
+        while(smallest[i]=='9' && smallest[i-1]=='9') i--;
+        smallest[i]-=1;
+        smallest[i-1]+=1;
+        return smallest;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int S,D;
+        cin >> S >> D;
+        Solution ob;
+        cout << ob.secondSmallest(S,D) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
